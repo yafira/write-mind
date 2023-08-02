695d372eb7b6697e707410b634fe138bed488d90
yafira <yafira.martinez@lc.cuny.edu>
2023-08-02 19:39:51 -0400

    edited h2 text

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
 	return (
 		<div className='app-main'>
 			<h1>mind write</h1>
-			<h2>a space to write what's on your mind, no strings attached.</h2>
+			<h2>a space to write what's on your mind, no commitments.</h2>
 			<textarea
 				id='message'
 				className='app-input'
