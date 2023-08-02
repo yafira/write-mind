8b54347329085d4bd6e75e3fa65359a23e2729df
yafira <yafira.martinez@lc.cuny.edu>
2023-08-02 18:18:04 -0400

    implemented text area and button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import React from 'react'
+import { useState } from 'react'
 import './style.css'
 
 function App() {
-	return <div className='App'></div>
+	const [userPrompt, setPrompt] = useState('')
+	const [valid, setValid] = useState(false)
+
+	const handleChange = (e) => {
+		const text = e.target.value
+		setPrompt(text)
+
+		if (!text.trim()) return setValid(false)
+		if (!text.match(/^[aA-zZ\s]+$/)) return setValid(false)
+		setValid(true)
+	}
+
+	return (
+		<div className='app-main'>
+			<h1>mind write</h1>
+			<h2>a space to write what's on your mind.</h2>
+			<textarea
+				className='app-input'
+				placeholder='what is on your mind?'
+				value={userPrompt}
+				onChange={handleChange}
+			/>
+			<button className='clay' disabled={!valid}>
+				Clear
+			</button>
+		</div>
+	)
 }
 
 export default App
