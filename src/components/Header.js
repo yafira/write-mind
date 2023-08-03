e8b17461bb9a1cae081dfc0972ddf4a9b76d030e
yafira <yafira.martinez@lc.cuny.edu>
2023-08-02 20:42:37 -0400

    edited header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,9 @@ export default function Header() {
 	return (
 		<div className='header'>
 			<h1>mind write</h1>
-			<h2>a space to write what's on your mind, no commitments.</h2>
+			<h2>
+				a space to <i>free</i> write what's on your mind
+			</h2>
 		</div>
 	)
 }
