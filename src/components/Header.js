2a785dd495ed9904a7e51439809088e5c20f6983
yafira <yafira.martinez@lc.cuny.edu>
2023-08-17 18:37:03 -0400

    updated name of app

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
 	return (
 		<div className='header'>
-			<h1>mind write</h1>
+			<h1>write mind</h1>
 			<h2>
 				a space to <i>free</i> write what's on your mind
 			</h2>
